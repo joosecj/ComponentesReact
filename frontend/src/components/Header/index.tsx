c2b963d367855e6bf02ac41561d57e41dab7ced6
Jose Carlos <josecarloscjj@gmail.com>
2022-12-06 17:02:55 -0300

    fix: created header

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function Header() {
+  return (
+    <header className="ct-header">
+      <nav className="ct-container">
+        <h1>DSCarrosTop</h1>
+      </nav>
+
+    </header>
+  );
+}
